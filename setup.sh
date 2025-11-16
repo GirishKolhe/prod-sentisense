1546d850249eefe4304eb23c393094a18696e6cb
GK <41370087+GirishKolhe@users.noreply.github.com>
2025-11-16 20:50:35 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,15 @@
 echo "installing nltk..."
 pip install nltk
 
+echo "installing scikit-learn..."
+pip install scikit-learn
+
 echo "Downloading and linking spacy..."
 pip install spacy
-# Install the exact model version that matches your spacy version
+
+echo "Downloading en_core_web_sm..."
 python -m spacy download en_core_web_sm
 # The 'download' command automatically runs 'link'
+
+echo "installing scipy..."
+pip install scipy
